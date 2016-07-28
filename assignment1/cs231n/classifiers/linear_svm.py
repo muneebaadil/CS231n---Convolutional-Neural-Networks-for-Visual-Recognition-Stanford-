SVM unvectorized loss function and gradient implemented, but not working correctly, although correct signs; wrong magnitudes

assignment1/cs231n/classifiers/linear_svm.py
@@ -26,14 +26,19 @@ def svm_loss_naive(W, X, y, reg):
   num_train = X.shape[0]
   loss = 0.0
   for i in xrange(num_train):
+    
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
     for j in xrange(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
+      toadd = np.zeros(W.shape)
       if margin > 0:
         loss += margin
+        toadd[:, j] = X[i] 
+        toadd[:, y[i]] = toadd[:, y[i]] - X[i]
+      dW = dW + toadd 
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -50,7 +55,7 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
+  dW = dW + 2 * W #Adding the gradient of regularization. 
 
   return loss, dW
 
@@ -88,5 +93,4 @@ def svm_loss_vectorized(W, X, y, reg):
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
-
   return loss, dW